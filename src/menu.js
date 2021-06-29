c90a4392b2ba4f8417b3adc687b0debd1ca69d40
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-06-29 21:19:05 +0200

    add template

---
+++ src/menu.js
@@ -0,0 +1,11 @@
+import menu from "./menu.json"
+import menuTemplate from "./menu.hbs"
+const menuItemBox = document.querySelector(".js-menu")
+
+
+function createMenu (menu) {
+  return menu.map(menuTemplate).join("")
+}
+
+
+menuItemBox.insertAdjacentHTML("beforeend", createMenu(menu))
